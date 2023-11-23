5ddd2999ff4d82884aaf67e09f4393ddd201621b
A01712317 <149627608+A01712317@users.noreply.github.com>
2023-11-23 12:51:19 -0600

    Update Clase_compuesta.h
    tiene comentarios para deja mas claro el codigo

--- Clase_compuesta.h
+++ Clase_compuesta.h
@@ -4,22 +4,27 @@
 using namespace std;
 #ifndef CLASE_COMPUESTA_H_
 #define CLASE_COMPUESTA_H_
-class mayoreo_vs_menudeo{
-    private:
-        mayoreo Mayoreo;
-        menudeo Menudeo;
-    public:
-        mayoreo_vs_menudeo(std::string n, float co, int c, float cap, float pm, float pp): Mayoreo(n, co, c, cap), Menudeo(n, co, c, cap) {
-            Mayoreo.setprecio(pm);
-            Menudeo.setprecio(pp);
-        }
+class mayoreo_vs_menudeo {
+private:
+    mayoreo Mayoreo;
+    menudeo Menudeo;
 
-        float getPrecioMenudeo() {
-            return Menudeo.getprecio();
-        }
+public:
+    // Constructor de la clase 'mayoreo_vs_menudeo'
+    mayoreo_vs_menudeo(std::string n, float co, int c, float cap, float pm, float pp) : Mayoreo(n, co, c, cap), Menudeo(n, co, c, cap) {
+        // Configuración de los precios al mayoreo y al menudeo
+        Mayoreo.setprecio(pm);
+        Menudeo.setprecio(pp);
+    }
 
-        float getPrecioMayoreo() {
-            return Mayoreo.getprecio();
-        }
+    // Método para obtener el precio al menudeo
+    float getPrecioMenudeo() {
+        return Menudeo.getprecio();
+    }
+
+    // Método para obtener el precio al mayoreo
+    float getPrecioMayoreo() {
+        return Mayoreo.getprecio();
+    }
 };
 #endif
